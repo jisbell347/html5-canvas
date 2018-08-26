ef207efbac14ca5423a57ecbc17714cf67e5cfef
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-25 19:27:50 -0600

    Added function that starts the line at a point on mouse down and ends on mouse up event.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector('#draw');
 // Defines what the dimensions of the canvas are.
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -12,3 +12,17 @@ ctx.lineCap = "round";
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+
+function draw(e) {
+	if(!isDrawing) return; //Stops the function from running when they are not moused down.
+	console.log(e);
+	ctx.beginPath();
+	ctx.moveTo(lastX, lastY);
+	ctx.lineTo(e.offsetX, e.offsetY);
+	ctx.stroke();
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
